ad0e9c142ed5ad3d93c503aa99ab9e5d22a386c5
Sttefanie Maciel <macielsttefanie@gmail.com>
2023-05-13 16:26:54 -0300

    configuracoes iniciais do projeto

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
+  it('passes', () => {
+    expect(true).toBe(true);
+  })
+
+});
